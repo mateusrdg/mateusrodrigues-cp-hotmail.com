d5f14722999f4df0daae3ee292b699f838a7ce89
mateusrdg <mateusrodrigues-cp@hotmail.comgit config --global user.emailgit config --global user.email mateusrodrigues-cp@hotmail.com>
2020-04-03 16:53:36 -0300

    Commit projeto

---
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,53 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+import java.util.List;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+
+public class LocacaoService {
+
+    private Double valorTotalFilmes = 0.0;
+
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws FilmeSemEstoqueException, LocadoraException {
+
+
+	    if(usuario == null) {
+			throw new LocadoraException("Usuario vazio");
+		}
+		
+		if(filmes == null || filmes.isEmpty()) {
+			throw new LocadoraException("Lista de filmes vazia");
+		}
+
+        for (Filme filme : filmes) {
+            if (filme.getEstoque() == 0) {
+                throw  new FilmeSemEstoqueException();
+            }
+            valorTotalFilmes += filme.getPrecoLocacao();
+        }
+
+		
+		Locacao locacao = new Locacao();
+		locacao.setFilmes(filmes);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(valorTotalFilmes);
+		
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+}
